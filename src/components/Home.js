a55067c8f9e3a7bae00c17ac96cc2f08ca6d0635
msajisaputra <majisaputra464@gmail.com>
2022-11-25 01:58:16 +0700

    Update file dan tambah folder

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,25 +1,18 @@
 import React from "react";
-import Image1 from "../assets/image/Image1.png";
-import Image2 from "../assets/image/Image2.png";
 
 const Home = () => {
   return (
     <div class="container">
       <div class="row">
-        <div class="col-md-auto">
+        <div class="col-md-6">
           <p class="text1">Kirim invoice lebih cepat dan mudah</p>
           <h1 class="text2">Software Invoice Untuk Semua Jenis Bisnis</h1>
-          <p class="text3"> 
+          <p class="text3">
             MyINVOICE merupakan software online yang berguna untuk membantu
             usahamu untuk membuat invoice, mengirim notifikasi pembayaran, serta
             menerima pembayaran dengan lebih cepat dan mudah!
           </p>
-
           <button class="button-lg-primary">Coba Gratis</button>
-        </div>
-        <div class="col-md-5">
-          <img class="my-image" src={Image1} alt="Logo" />
-          <img class="my-image" src={Image2} alt="Logo" />
         </div>
       </div>
     </div>
