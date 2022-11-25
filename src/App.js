251434aa34905cda0d0250bbee253da9f5d8c83d
msajisaputra <majisaputra464@gmail.com>
2022-11-25 11:13:27 +0700

    Update file folder

--- src/App.js
+++ src/App.js
@@ -2,14 +2,15 @@ import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.min.js";
 import "./App.css";
-import { Navbar, Home, Fitur } from "./components";
+import { Navbar, Home, Footer } from "./components";
+
 
 function App() {
   return (
     <div className="App">
       <Navbar />
       <Home />
-      <Fitur />
+      <Footer />
     </div>
   );
 }
--- src/components/Fitur.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Fitur = () => {
-  return (
-    <div>Fitur</div>
-  )
-}
-
-export default Fitur;
