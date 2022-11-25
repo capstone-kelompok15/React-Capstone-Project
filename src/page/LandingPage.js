efe3293358685b469567ba8433f8595e36cec510
Abdullah Nouval Shidqi <87747795+AbdullahNouvalShidqi@users.noreply.github.com>
2022-11-25 11:59:21 +0700

    development: footer added to landing page

--- src/page/LandingPage.js
+++ src/page/LandingPage.js
@@ -2,6 +2,7 @@ import React from 'react'
 import Navbar from "../components/Navbar";
 import Home from "../components/Home";
 import Fitur from "../components/Fitur";
+import Footer from '../components/Footer'
 
 const LandingPage = () => {
   return (
@@ -9,6 +10,7 @@ const LandingPage = () => {
         <Navbar />
         <Home />
         <Fitur />
+        <Footer/>
     </div>
   )
 }
