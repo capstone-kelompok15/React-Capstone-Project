3287f198b15773c12da2dc1b9065d6f1c86cf687
Abdullah Nouval Shidqi <87747795+AbdullahNouvalShidqi@users.noreply.github.com>
2022-11-25 12:04:03 +0700

    development: adding background color to footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import Symbol1 from "../assets/image/Symbol1.png";
 
 const Footer = () => {
   return (
-    <div class="container">
+    <div class="container-fluid footer">
       <div class="row">
         <div class="col-12 text-center">
           <img
