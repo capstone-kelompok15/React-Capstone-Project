562791a39567f66834c4197f279215130cfb3eb0
Abdullah Nouval Shidqi <87747795+AbdullahNouvalShidqi@users.noreply.github.com>
2022-11-26 10:50:31 +0700

    register_login_slice: forgot password and button primary fixed

--- src/components/loginPage/LoginPageBody.js
+++ src/components/loginPage/LoginPageBody.js
@@ -10,13 +10,17 @@ const LoginPageBody = () => {
                     <Form>
                         <Form.Group className="mt-4" style={{width: '400px'}}>
                             <Form.Label>Username</Form.Label>
-                            <Form.Control name="email" type="email" placeholder="Enter username" />
+                            <Form.Control name="username" type="text" placeholder="Enter username" />
                         </Form.Group>
                         <Form.Group className="mt-4">
                             <Form.Label>Password</Form.Label>
                             <Form.Control name='password' type="password" placeholder="Enter password" />
                         </Form.Group>
-                        <div className='d-flex justify-content-end mt-2 forgot-password-text'>Forget password?</div>
+                        <div className='d-flex justify-content-end mt-2'>
+                            <div className="forgot-password-text">
+                                Forget password?
+                            </div>
+                        </div>
                     </Form>
                     <div className="button-primary d-flex justify-content-center mt-5">Login</div>
                 </Col>
