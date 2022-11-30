b5c9ded13da64e0090909665141c32c4b70f7687
Abdullah Nouval Shidqi <87747795+AbdullahNouvalShidqi@users.noreply.github.com>
2022-11-30 18:58:47 +0700

    development: fixing files

--- src/components/dashboardPage/Dashboard.js
+++ src/components/dashboardPage/Dashboard.js
@@ -13,45 +13,43 @@ const Dashboard = () => {
   return (
 
     <main className={show ? 'space-toggle' : null}>
-      <header className={`header ${show ? 'space-toggle' : null}`}>
-        <div className='header-toggle' onClick={() => setShow(!show)}>
-          <i className={`fas fa-bars ${show ? 'fa-solid fa-xmark' : null}`}></i>
-        </div>
+      <header className={`header`}>
+        
       </header>
 
       <aside className={`sidebar ${show ? 'show' : null}`}>
         <nav className='nav'>
           <div>
             <a href='/' className='navbar-brand2'>
-			<img
-            src={Symbol}
-            alt="Logo"
-            width="22"
-            height="22"
-            className="img-fluid me-2"
-          />
-          My<span className="span-invoice">INVOICE</span>
+              <img
+                src={Symbol}
+                alt="Logo"
+                width="22"
+                height="22"
+                className="img-fluid me-2"
+              />
+              My<span className="span-invoice">INVOICE</span>
             </a>
 
             <div className='nav-list'>
-              <a href='#' className='nav-item2 active2'>
-			  <img className="logo4 ms-5 ps-2 me-2" src={dashboard} alt="Logo" />
+              <a href='#' className='nav-item2 active2 mx-5'>
+			          <img className="logo4 ps-2 me-2" src={dashboard} alt="Logo" />
                 <span className='nav-item2-name'>Dashboard</span>
               </a>
-              <a href='#' className='nav-item2'>
-			  <img className="logo4 ms-5 ps-2 me-2" src={invoice} alt="Logo" />
+              <a href='#' className='nav-item2 mx-5'>
+			          <img className="logo4 ps-2 me-2" src={invoice} alt="Logo" />
                 <span className='nav-item2-name'>Invoices</span>
               </a>
-              <a href='#' className='nav-item2'>
-			  <img className="logo4 ms-5 ps-2 me-2" src={payment} alt="Logo" />
+              <a href='#' className='nav-item2 mx-5'>
+			          <img className="logo4 ps-2 me-2" src={payment} alt="Logo" />
                 <span className='nav-item2-name'>Payments</span>
               </a>
-              <a href='#' className='nav-item2'>
-			  <img className="logo4 ms-5 ps-2 me-2" src={customer} alt="Logo" />
+              <a href='#' className='nav-item2 mx-5'>
+			          <img className="logo4 ps-2 me-2" src={customer} alt="Logo" />
                 <span className='nav-item2-name'>Customer</span>
               </a>
-			  <a href='#' className='nav-item2'>
-			  <img className="logo4 ms-5 ps-2 me-2" src={profile} alt="Logo" />
+			        <a href='#' className='nav-item2 mx-5'>
+			          <img className="logo4 ps-2 me-2" src={profile} alt="Logo" />
                 <span className='nav-item2-name'>Profile</span>
               </a>
             </div>
--- src/page/DashboardPage.js
+++ src/page/DashboardPage.js
@@ -1,18 +1,9 @@
-<<<<<<< Updated upstream
-import Dashboard from "../components/dashboardpage_components/Dashboard"
-
-const DashboardPage = () => {
-  return (
-    <Dashboard/>
-  )
-=======
-import Dashboard from "../components/dashboardpage_components/Dashboard";
+import Dashboard from "../components/dashboardPage/Dashboard";
 
 const DashboardPage = () => {
     return(
         <Dashboard/>
     );
->>>>>>> Stashed changes
 }
 
 export default DashboardPage;
