86cd87e64b2ed571b7fcaf9e58ef96e61fe47e4d
Abdullah Nouval Shidqi <87747795+AbdullahNouvalShidqi@users.noreply.github.com>
2022-12-05 11:31:04 +0700

    invoicesPage-slice: slicing 80% done

--- src/components/invoicePage/InvoiceCard.js
+++ src/components/invoicePage/InvoiceCard.js
@@ -1,24 +1,27 @@
 import { Col, Row } from "react-bootstrap";
 import arrowIcon from '../../assets/svg/arrowIcon.svg'
 import documentIcon from '../../assets/svg/documentIcon.svg'
+import formatRupiah from "../../utils/formatRupiah";
 
 const InvoiceCard = (props) => {
-    const { invoiceData } = props;
+    const { invoiceData, onClick, selected } = props;
+
+    console.log(invoiceData.customer);
 
     return(
-        <Row fluid className='invoice-card align-items-center' style={{padding: '10px 0px 10px 40px', borderBottom: '1px solid #CDCDCD'}}>
-            <Col md={1}>
+        <Row fluid className={`invoice-card align-items-center m-0 ${selected ? 'selected' : ""}`} style={{padding: '10px 0px 10px 40px', borderBottom: '1px solid #CDCDCD'}} onClick={onClick}>
+            <Col md={1} className='p-0'>
                 <img src={documentIcon} alt={'Not Found'}/>
             </Col>
-            <Col md={5}>
-                <div className="invoice-card-title">{invoiceData.name}</div>
-                <div className="invoice-card-subtitle">{invoiceData.invoiceId} | {invoiceData.dateTime}</div>
+            <Col md={5} className='p-0'>
+                <div className="invoice-card-title">{invoiceData.customer.name}</div>
+                <div className="invoice-card-subtitle">{invoiceData.id} | {invoiceData.created_at}</div>
             </Col>
-            <Col md={4}>
-                <div className="invoice-card-price">{invoiceData.price}</div>
-                <div className="invoice-card-status">{invoiceData.due}</div>
+            <Col md={5} className='p-0'>
+                <div className="invoice-card-price">{formatRupiah(invoiceData.total_price)}</div>
+                <div className="invoice-card-status">{invoiceData.due_at}</div>
             </Col>
-            <Col md={1}>
+            <Col md={1} className='p-0'>
                 <img src={arrowIcon} alt='not found'/>
             </Col>
         </Row>
--- src/components/invoicePage/InvoicesPageBody.js
+++ src/components/invoicePage/InvoicesPageBody.js
@@ -1,14 +1,22 @@
 import { Col, Container, Row } from "react-bootstrap";
 import { AiFillPlusCircle } from 'react-icons/ai';
 import filterIcon from '../../assets/svg/filterIcon.svg';
-import personPointing from '../../assets/svg/personPointing.svg';
 import InvoiceCard from "./InvoiceCard";
+import NoSelectedList from "./NoSelectedList";
+import InvoiceDetail from "./InvoiceDetail";
+import { useState } from "react";
 
 const InvoicesPageBody = () => {
+    const [ detailData, setDetailData ] = useState(undefined);
+
+    const invoiceCardOnClick = (detailData) => {
+        setDetailData(detailData);
+    }
+
     return(
         <>
-            <Row>
-                <Col md>
+            <Row className='m-0'>
+                <Col md={6} className='p-0'>
                     <Container fluid className="p-0" style={{backgroundColor: 'white', height: 'calc(100vh - 65px)', borderRight: '2px solid #CDCDCD'}}>
                         <div className="d-flex flex-row justify-content-between align-items-center" style={{padding: '40px 40px 10px'}}>
                             <div className='invoice-title'>All Invoices</div>
@@ -20,15 +28,13 @@ const InvoicesPageBody = () => {
                                 <img className="filter-invoice" src={filterIcon} alt="Test"/>
                             </div>
                         </div>
-                        <Container fluid className="invoice-cards-container p-0" style={{height: 'calc(100vh - 150px)', overflowX: 'hidden'}}>
-                            {DATA_DUMY.map(data => <InvoiceCard invoiceData={data}/>)}
+                        <Container fluid className="invoice-cards-container p-0" style={{height: 'calc(100vh - 150px)', overflow:'auto'}}>
+                            {DATA_DUMY.map(data => <InvoiceCard invoiceData={data} onClick={() => {invoiceCardOnClick(data)}} selected={data.id === detailData.id}/>)}
                         </Container>
                     </Container>
                 </Col>
-                <Col md className="d-flex flex-column justify-content-center align-items-center">
-                    <img src={personPointing} alt='not found'/>
-                    <div className="no-selected-list-text">You can select one invoice from list to view</div>
-                </Col>
+                {detailData === undefined ? <NoSelectedList/> : <InvoiceDetail detailData={detailData}/>}
+                
             </Row>
         </>
     );
@@ -38,73 +44,109 @@ export default InvoicesPageBody;
 
 const DATA_DUMY = [
     {
-        name: 'Alvin Wiraprathama',
-        invoiceId: 'INV-00341212',
-        dateTime: '03 Dec 2022',
-        price: 'Rp. 2.200.000',
-        due: 'Overdue by 3 Days'
+        id: 'INV-00341212',
+        payment_status: "Unpaid",
+        total_price: 24800000,
+        due_at: "28 Nov 2022",
+        created_at : "24 Nov 2022",
+        updated_at: "",
+        merchant: {
+            id: "string",
+            name: "string",
+            display_profile_url: "string",
+            address: "string"
+        },
+        customer: {
+            id: "1231233",
+            name: "Alvin Wiraprathama",
+            email: "wiraprathamaalvin@gmail.com",
+            address: "Jalan Gajah Waktra no 1"
+        },
+        "payment_method": {
+            "payment_type": "manual",
+            "bank_name": "string",
+            "bank_code": "string",
+            "bank_number": "string",
+            "on_behalf_of": "string"
+        },
+        items: [
+            {
+            product: "Asus Vivobook",
+            quantity: 2,
+            price: 12000000,
+            total_price: 24000000,
+            created_at: "string",
+            updated_at: "string"
+            },
+            {
+            product: "SSD Samsung Evo",
+            quantity: 1,
+            price: 800000,
+            total_price: 800000,
+            created_at: "string",
+            updated_at: "string"
+            },
+            {
+            product: "Shipping",
+            quantity: 1,
+            price: 0,
+            total_price: 0,
+            created_at: "string",
+            updated_at: "string"
+            },
+        ]
     },
     {
-        name: 'Alvin Wiraprathama',
-        invoiceId: 'INV-00341212',
-        dateTime: '03 Dec 2022',
-        price: 'Rp. 2.200.000',
-        due: 'Overdue by 3 Days'
-    },
-    {
-        name: 'Alvin Wiraprathama',
-        invoiceId: 'INV-00341212',
-        dateTime: '03 Dec 2022',
-        price: 'Rp. 2.200.000',
-        due: 'Overdue by 3 Days'
-    },
-    {
-        name: 'Alvin Wiraprathama',
-        invoiceId: 'INV-00341212',
-        dateTime: '03 Dec 2022',
-        price: 'Rp. 2.200.000',
-        due: 'Overdue by 3 Days'
-    },
-    {
-        name: 'Alvin Wiraprathama',
-        invoiceId: 'INV-00341212',
-        dateTime: '03 Dec 2022',
-        price: 'Rp. 2.200.000',
-        due: 'Overdue by 3 Days'
-    },
-    {
-        name: 'Alvin Wiraprathama',
-        invoiceId: 'INV-00341212',
-        dateTime: '03 Dec 2022',
-        price: 'Rp. 2.200.000',
-        due: 'Overdue by 3 Days'
-    },
-    {
-        name: 'Alvin Wiraprathama',
-        invoiceId: 'INV-00341212',
-        dateTime: '03 Dec 2022',
-        price: 'Rp. 2.200.000',
-        due: 'Overdue by 3 Days'
-    },
-    {
-        name: 'Alvin Wiraprathama',
-        invoiceId: 'INV-00341212',
-        dateTime: '03 Dec 2022',
-        price: 'Rp. 2.200.000',
-        due: 'Overdue by 3 Days'
-    },
-    {
-        name: 'Alvin Wiraprathama',
-        invoiceId: 'INV-00341212',
-        dateTime: '03 Dec 2022',
-        price: 'Rp. 2.200.000',
-        due: 'Overdue by 3 Days'
-    },
-    {
-        name: 'Alvin Wiraprathama',
-        invoiceId: 'INV-00341212',
-        dateTime: '03 Dec 2022',
-        price: 'Rp. 2.200.000',
-        due: 'Overdue by 3 Days'
+        id: 'INV-00341213',
+        payment_status: "Unpaid",
+        total_price: 20000000,
+        due_at: "28 Nov 2022",
+        created_at : "24 Nov 2022",
+        updated_at: "",
+        merchant: {
+            id: "string",
+            name: "string",
+            display_profile_url: "string",
+            address: "string"
+        },
+        customer: {
+            id: "1231234",
+            name: "Abdullah Nouval Shidqi",
+            email: "abullah.nouval@gmail.com",
+            address: "Jalan Kawista no 2"
+        },
+        "payment_method": {
+            "payment_type": "manual",
+            "bank_name": "string",
+            "bank_code": "string",
+            "bank_number": "string",
+            "on_behalf_of": "string"
+        },
+        items: [
+            {
+            product: "Microsoft Surface Pro 4",
+            quantity: 1,
+            price: 12000000,
+            total_price: 12000000,
+            created_at: "string",
+            updated_at: "string"
+            },
+            {
+            product: "SSD Seagate",
+            quantity: 1,
+            price: 800000,
+            total_price: 800000,
+            created_at: "string",
+            updated_at: "string"
+            },
+            {
+            product: "Shipping",
+            quantity: 1,
+            price: 0,
+            total_price: 0,
+            created_at: "string",
+            updated_at: "string"
+            },
+        ]
     },
 ]
